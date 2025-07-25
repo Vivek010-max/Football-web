5a8780ff7c32b5a6d716ca7a3b137ef1543e8102
Vivek010-max <vy464063@gmail.com>
2025-07-25 22:11:28 +0530

    navbar

---
+++ football/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-center p-4 justify-center"> FOOTBALL</h1>
+
+     </main>
+  );
+}

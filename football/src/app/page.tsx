d2e07e757f1871f74847cbb1b1b14992a85fd652
Vivek010-max <vy464063@gmail.com>
2025-07-25 23:30:29 +0530

    Hero section added

--- football/src/app/page.tsx
+++ football/src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-center p-4 justify-center"> FOOTBALL</h1>
+      < Hero/>
 
      </main>
   );

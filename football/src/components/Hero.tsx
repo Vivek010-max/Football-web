0cbec3fe8399ca1cc5b4f76d5f6160050a341d04
Vivek010-max <vy464063@gmail.com>
2025-07-26 09:35:15 +0530

    spotlight

--- football/src/components/Hero.tsx
+++ football/src/components/Hero.tsx
@@ -1,11 +1,16 @@
 "use client";
 import React from "react";
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import { Spotlight } from "@/components/ui/Spotlight"
 
 export default function Hero() {
   return (
     <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
+        <Spotlight
+        className="-top-40 left-0 md:-top-20 md:left-60"
+        fill="white"
+        />
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           Feel The Pulse Football Redefined 
         </h1>
